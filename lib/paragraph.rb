25072546b4bf6947b86dcce22d2f173b215e491d
Larisasmiles <larisasmiles@gmail.com>
2017-04-13 11:29:47 -0600

    Working on TDD

---
+++ lib/paragraph.rb
@@ -0,0 +1,19 @@
+class Paragraph
+  attr_reader :text, :html_format
+
+  def initialize(text)
+    @text = text
+    @html_format = paragraph_markdown
+  end
+
+  def paragraph_markdown
+    if text.include?("\n")
+      middle_text = text.strip
+      "<p>\n#{middle_text}\n</p>\n"
+    else
+      middle_text = text.strip
+      "<p>#{middle_text}</p>"
+    end
+  end
+  
+end
---
+++ test/paragraph_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/paragraph.rb'
+
+class ParagraphTest < Minitest::Test
+
+  def test_if_it_exists
+    assert Paragraph.new("text")
+  end
+
+  def test_if_paragraph_converts
+    p = Paragraph.new("This is the first line of the paragraph.")
+    assert_equal "<p>This is the first line of the paragraph.</p>", p.html_format
+  end
+  
+  def test_mulitple_lines_without_new_line_character
+    p = Paragraph.new("You just have to try the cheesecake, he said. Ever since it appeared in Food & Wine this place has been packed every night.")
+    assert_equal "<p>You just have to try the cheesecake, he said. Ever since it appeared in Food & Wine this place has been packed every night.</p>", p.html_format
+  end
+
+  def test_mulitple_lines_with_new_line_characters
+    p = Paragraph.new("You just have to try the cheesecake, he said. Ever since it appeared in Food & Wine this place has been packed every night.\n")
+    assert_equal "<p>You just have to try the cheesecake, he said. Ever since it appeared in Food & Wine this place has been packed every night.</p>", p.html_format
+  end
+end
