56bffea7f45c4f9268746e42bdf61d5749067380
Larisasmiles <larisasmiles@gmail.com>
2017-04-13 17:55:15 -0600

    TDD passing

--- lib/formatting.rb
+++ lib/formatting.rb
@@ -1,36 +1,42 @@
 class Formatting
-  attr_reader :text
+
+  attr_reader :text, :html_format
 
   def initialize(text)
     @text = text
-    # @html_format = formatting_converter
+    @html_format = formatting_converter
   end
 
-  # def formatting_converter
-  #   if amp
-  #     amp_converter
-  #   elsif bold?
-  #     bold_converter
-  #   else
-  #    italics_converter
-  #   end
-  # end
-  
-  def italics_converter(text)
+  def formatting_converter
+    if amp?
+      amp_converter
+    elsif bold?
+      bold_converter
+    else
+      italics_converter
+    end
+  end
+
+  def amp?
+    text.include?("&")
+  end
+
+  def bold?
+    text.include?("**")
+  end
+
+  def amp_converter
+    text.gsub("&", "&amp;")
+  end
+
+  def bold_converter
+    middle_text = text.delete("*").strip
+    "<strong>#{middle_text}</strong>"
+  end
+
+  def italics_converter
     middle_text = text.delete("*").delete("_").strip
     "<em>#{middle_text}</em>"
   end
 
-  def bold_converter(text)
-    middle_text = text.delete("**").strip
-    "<strong>#{middle_text}</strong>"
-  end
-  
-  def amp_converter(text)
-    text.gsub("&", "&amp")
-  end
-  
 end
-
-
-  
--- test/formatting_test.rb
+++ test/formatting_test.rb
@@ -4,32 +4,44 @@ require './lib/formatting.rb'
 
 class FormattingTest < Minitest::Test
 
-  def test_if_it_exists
+  def test_it_exists
 
-   assert Formatting.new("text")
-  end
-  
-  def test_if_it_coverts_to_italics
-    f = Formatting.new("You just *have* to try the cheesecake")
-    output = f.italics_converter(f.text)
-
-    assert_equal "<em>You just have to try the cheesecake</em>", output
+    assert FormattingTest.new('<strong>#{middle_text}</strong>')
   end
 
-  def test_if_it_converts_to_bold
-    f = Formatting.new("Ever since it appeared in **Food and Wine** this place has been packed every night.")
-    output = f.bold_converter(f.text)
-    
+  def test_convert_italics
+    text = Formatting.new("*You just *have* to try cheesecake")
 
-    assert_equal "Ever since it appeared in <strong>Food and Wine</strong> this place has been packed every night.", output
+    assert_equal '<em>You just have to try cheesecake</em>', text.formatting_converter
   end
-  
-  def test_if_it_converts_to_amp
-    f = Formatting.new("Ever since it appeared in Food & Wine")
-    output = f.amp_converter(f.text)
 
-    assert_equal "Ever since it appeared in Food &amp Wine", output
+  def test_convert_bold
+    text = Formatting.new("**Food and Wine**")
+
+    assert_equal '<strong>Food and Wine</strong>', text.formatting_converter
   end
-  
+
+  def test_convert_amp
+    string = Formatting.new("Food & Wine")
+
+    assert_equal "Food &amp; Wine", string.html_format
+  end
+
+  def test_bold_with_new_line_characters
+    text = Formatting.new("**Food and Wine**\n")
+
+    assert_equal "<strong>Food and Wine</strong>", text.formatting_converter
+  end
+
+  def test_italics_with_new_line_character
+    text = Formatting.new("You just *have* to try the cheesecake\n")
+
+    assert_equal "<em>You just have to try the cheesecake</em>", text.formatting_converter
+  end
+
+  def test_convert_amp_with_new_line_character
+    text = Formatting.new("Food & Wine\n")
+
+    assert_equal "Food &amp; Wine\n", text.formatting_converter
+  end
 end
-
