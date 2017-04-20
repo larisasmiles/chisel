f6b3bb1d1222cceaf68f14c5c21b608eea2d6690
Larisasmiles <larisasmiles@gmail.com>
2017-04-19 23:33:56 -0600

    final commit

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,16 +1,20 @@
-require "./lib/paragraph.rb"
+  
+class Chisel
+# require "./lib/paragraph.rb"
+
+require 'pry'; binding.pry
 
 
 input_file = File.open(ARGV[0], "r") 
-incoming_text = input_file.read
-input_file.close
+# incoming_text = input_file.read
+# input_file.close
 
-my_output = Paragraph.new(incoming_text)
-writer = File.open(ARGV[1], "w")
-writer.write(my_output)
-writer.close
+# my_output = Paragraph.new(incoming_text)
+# writer = File.open(ARGV[1], "w")
+# writer.write(my_output)
+# writer.close
 
-
+end
 
 
 
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -2,10 +2,12 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/chisel.rb"
 
-class ChiselTest <Minitest::Test
+class ChiselTest < Minitest::Test
   
-  def test_if_it_exist
-    assert Chisel
+  def test_if_input_file_exists
+    input_file = ARGV[0]
+
+    assert input_file
   end
 end
 
